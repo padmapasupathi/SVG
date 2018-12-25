ade08053dec846552b6c29d494367faded79565e
padmapasupathi <46036234+padmapasupathi@users.noreply.github.com>
2018-12-25 17:21:28 -0500

    Happy Birthday Greetings
     Greeting card with cake and Butterfly with "Happy Birthday" wishes

---
+++ BirthdayGreetings.elm
@@ -0,0 +1,58 @@
+module Main exposing (main)
+
+import GraphicSVG exposing (..)
+import GraphicSVG.EllieApp exposing (GetKeyState,gameApp)
+
+init = { time = 0 }
+
+type Msg = Tick Float GetKeyState
+  
+update msg model =
+    case msg of
+        Tick t _ -> { model | time = t }
+
+view model =
+    collage 500 500 (myGame model)
+    
+myGame model = 
+    [ 
+    {-Frame-}
+        rect 300 300 |> filled (rgb 255 255 180)
+    {-Cake-}
+        , roundedRect 80 45 15  |> filled pink  |> move (0,20)
+        , rect 80 45 |> filled hotPink  |> move (0,0)
+        , rect 80 15 |> filled pink  |> move (0,0)
+        , rect 5 25 |> filled hotPink  |> move (0,40)
+        , oval 7 10 |> filled orange |> move(0,57) |> scale (sin(3*model.time))
+        , circle 5 |> filled green |> move (0,0)
+        , circle 5 |> filled green |> move (25,0)
+        , circle 5 |> filled green |> move (-25,0)
+        , circle 5 |> filled green |> move (13,15)
+        , circle 5 |> filled green |> move (-13,15)
+        , circle 5 |> filled green |> move (-35,15)
+        , circle 5 |> filled green |> move (35,15)
+        , circle 5 |> filled green |> move (13,-15)
+        , circle 5 |> filled green |> move (-13,-15)
+        , circle 5 |> filled green |> move (-35,-15)
+        , circle 5 |> filled green |> move (35,-15)
+    {-Butterfly-}
+        , oval 50 27 |> filled pink |> move (80,80) |> rotate (degrees 210) |> rotate (degrees <| 12.5 * sin(2*model.time))
+        , oval 50 27 |> filled pink |> move (80,80) |> rotate (degrees 135) |> rotate (degrees <| 12.5 * cos(2*model.time))
+        , oval 50 7 |> filled green |> move (80,80) |> rotate (degrees 75) |> rotate (degrees <| 12.5 * sin(2*model.time))
+        , oval 50 27 |> filled pink |> move (-80,-80) |> rotate (degrees 210) |> rotate (degrees <| 12.5 * sin(2*model.time))
+        , oval 50 27 |> filled pink |> move (-80,-80) |> rotate (degrees 135) |> rotate (degrees <| 12.5 * cos(2*model.time))
+        , oval 50 7 |> filled green |> move (-80,-80) |> rotate (degrees 75) |> rotate (degrees <| 12.5 * sin(2*model.time))
+     {-Text-}
+        , text "Happy Birthday" |> filled blue |> move (-100,100) |> scale 2 
+        , text "Enjoy the day" |> filled blue |> move (0,-100) |> scale 2
+        ] 
+ 
+main =
+    gameApp
+        Tick
+        { 
+            model = init
+        ,   view = view
+        ,   update = update
+        ,   title = "Happy Birthday Card" -- This is the title of the browser window / tab
+        }
